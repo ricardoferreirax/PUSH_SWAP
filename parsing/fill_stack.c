5ef262cb0eb0d068d11b34cd0766a2b7e1e190be
rickymercury <ricardomedeirosx@gmail.com>
2025-06-20 16:39:46 +0100

    Push Swap v.1.3 - add fill_stack

--- parsing/fill_stack.c
+++ parsing/fill_stack.c
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 23:13:35 by rmedeiro          #+#    #+#             */
-/*   Updated: 2025/06/15 23:20:56 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2025/06/20 16:35:48 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,33 +58,5 @@ void	ft_fill_stack(char **str, t_stack **stack)
 		value = ft_atoi(str[i]);
 		ft_lst_add_back(stack, ft_lst_new(value));
 		i++;
-	}
-}
-
-void	ft_index_stack(t_stack *stack_a, int size)
-{
-	t_stack	*temp;
-	t_stack	*high;
-	int		value;
-
-	while (size-- > 0)
-	{
-		temp = stack_a;
-		value = INT_MIN;
-		high = NULL;
-		while (temp)
-		{
-			if (temp->value == INT_MIN && temp->index == 0)
-				temp->index = 1;
-			if (temp->value > value && temp->index == 0)
-			{
-				value = temp->value;
-				high = temp;
-			}
-			else
-				temp = temp->next;
-		}
-		if (high)
-			high->index = size;
 	}
 }
