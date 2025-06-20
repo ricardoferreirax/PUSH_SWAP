5a7c4f210d322a50cabc60a4e691ea3301c96033
rickymercury <ricardomedeirosx@gmail.com>
2025-06-20 16:51:33 +0100

    Push Swap v1.4 - add parsing_stack on parsing directory

--- parsing/parsing_stack.c
+++ parsing/parsing_stack.c
@@ -6,17 +6,20 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 13:09:41 by rmedeiro          #+#    #+#             */
-/*   Updated: 2025/06/20 16:37:22 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2025/06/20 16:50:56 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-static int	ft_is_full_space(char *s)
+static int	ft_has_only_spaces(char *s)
 {
-	while (*s == ' ')
-		s++;
-	if (*s == '\0')
+	int	i;
+
+	i = 0;
+	while (s[i] == ' ')
+		i++;
+	if (s[i] == '\0')
 		return (1);
 	return (0);
 }
@@ -31,7 +34,7 @@ static char	*ft_join_args(char **args)
 	tmp = ft_strdup("");
 	while (args[i])
 	{
-		if (ft_is_full_space(args[i]))
+		if (ft_has_only_spaces(args[i]))
 		{
 			ft_error();
 			free(tmp);
@@ -46,7 +49,7 @@ static char	*ft_join_args(char **args)
 	return (tmp);
 }
 
-char	**ft_stack(char **args)
+char	**ft_parsing_stack(char **args)
 {
 	char	**str;
 	char	*s;
