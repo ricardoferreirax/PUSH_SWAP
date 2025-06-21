cf9aadeac34900746c10ae7d0a1761877953eba4
rickymercury <ricardomedeirosx@gmail.com>
2025-06-21 13:59:10 +0100

    Push Swap v2.0 - swap operation

--- src/operations/swap.c
+++ src/operations/swap.c
@@ -6,11 +6,11 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 23:24:08 by rmedeiro          #+#    #+#             */
-/*   Updated: 2025/06/21 13:14:24 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2025/06/21 13:58:59 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/push_swap.h"
+#include "../../includes/push_swap.h"
 
 static void	ft_swap(t_stack *stack)
 {
