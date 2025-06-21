ee2f8f31e550289d5896f57a1e612acb0134b1f2
rickymercury <ricardomedeirosx@gmail.com>
2025-06-21 14:02:45 +0100

    Push Swap v2.0 - cost and cheapest sorting

--- src/sorting/cost.c
+++ src/sorting/cost.c
@@ -6,11 +6,11 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 23:22:00 by rmedeiro          #+#    #+#             */
-/*   Updated: 2025/06/20 23:57:41 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2025/06/21 14:02:18 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/push_swap.h"
+#include "../../includes/push_swap.h"
 
 void	ft_cost(t_stack **stack_a, t_stack **stack_b)
 {
